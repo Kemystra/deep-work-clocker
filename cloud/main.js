7a821aacdcebb1bc70001863a495b427ce95490d
Izzminhal Akmal <izzmin97@gmail.com>
2023-02-01 16:18:18 +0800

    Deleted cloud function add_hours

--- cloud/main.js
+++ cloud/main.js
@@ -4,18 +4,3 @@
 Parse.Cloud.define("hello", (request) => {
 	return "Hello world!";
 });
-
-Parse.Cloud.define("add_hours", async request => {
-	const query = new Parse.Query("DeepHour");
-
-	// Get only the object with that were created today
-	// Setting the hours right at midnight
-	query.greaterThan((new Date()).setHours(0,0,0,0));
-
-	try {
-		let result = await query.find();
-		console.log(result);
-	} catch (error) {
-		console.error(error);
-	}
-});
