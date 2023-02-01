1666d50ab8b393cb0b5e9e24a5143a77853e451b
Izzminhal Akmal <izzmin97@gmail.com>
2023-02-01 10:02:14 +0800

    Initialized Parse

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import Parse from 'parse/node'
+
+import "../scss/master.scss"
+
+Parse.initialize(
+    "c2CLmzJBHw8cPD6gvehLa5FwUjuCgHtBxWiUhU25", // App. ID
+    "LdSZCyRhg7nn7rYNw8c5TylpOHaxeF5uqYgjeLtT" // JS key
+);
+
+Parse.serverURL = "https://parseapi.back4app.com/";
