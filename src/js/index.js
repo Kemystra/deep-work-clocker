e7117e5e9dbaf3484e55119d5ce86e568c0d1f72
Izzminhal Akmal <izzmin97@gmail.com>
2023-02-01 11:22:48 +0800

    Implemented basic login

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,18 @@ Parse.initialize(
 );
 
 Parse.serverURL = "https://parseapi.back4app.com/";
+
+document.getElementById("login-form").onsubmit = async e => {
+    e.preventDefault();
+
+    let username = e.target["0"].value;
+    let passwd = e.target["1"].value;
+
+    try {
+        let user = await Parse.User.logIn(username, passwd);
+        console.log("Logged in!");
+        console.table(user);
+    } catch (error) {        
+        console.error(error);
+    }
+}
