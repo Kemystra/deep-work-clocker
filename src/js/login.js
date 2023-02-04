e9ce130a75b82e2127e131e3e2d96c15c39f5907
Izzminhal Akmal <izzmin97@gmail.com>
2023-02-04 20:39:44 +0800

    Fixed a lot of bugs LOL

--- src/js/login.js
+++ src/js/login.js
@@ -1,6 +1,6 @@
 import { hideLoginPanel } from "./hide_login.js";
 
-export async function normalLogin() {
+export async function normalLogin(e) {
     e.preventDefault();
 
     // Get the credentials from the elements
@@ -8,14 +8,17 @@ export async function normalLogin() {
     let passwd = e.target["1"].value;
 
     try {
-        let user = await Parse.User.logIn(username, passwd);
+        // Set the HTTP method to POST to hide from URL search bar
+        let user = await Parse.User.logIn(username, passwd, {
+            method: 'POST'
+        });
         console.log("Logged in!");
         console.table(user);
 
         // Save user's token to cookie
-        let sessionToken = Parse.Session.getSessionToken();
-        console.log(Parse.Session.createdAt);
-        document.cookie = `SameSite=strict;expires=${Parse.Session.createdAt.toUTCString()};userToken=${sessionToken}`
+        let sessionToken = user.getSessionToken();
+        console.log(user.createdAt);
+        document.cookie = `SameSite=strict;expires=${user.createdAt.toUTCString()};userToken=${sessionToken}`
 
         // Hide panel after finished
         hideLoginPanel();
@@ -28,7 +31,9 @@ export async function normalLogin() {
 export async function sessionTokenLogin(token) {
     try {
         const user = await Parse.User.become(token);
+        console.log("Logged in with the previous session token");
     } catch (error) {
         console.error(error);
+        console.log("Error on using previous session token");
     }
 }
