8e756120cfe6377a698de766d9ed93cf347191d1
Izzminhal Akmal <izzmin97@gmail.com>
2023-02-04 19:34:10 +0800

    Completed both functions

--- src/js/login.js
+++ src/js/login.js
@@ -1,6 +1,9 @@
+import { hideLoginPanel } from "./hide_login.js";
+
 export async function normalLogin() {
     e.preventDefault();
 
+    // Get the credentials from the elements
     let username = e.target["0"].value;
     let passwd = e.target["1"].value;
 
@@ -10,9 +13,22 @@ export async function normalLogin() {
         console.table(user);
 
         // Save user's token to cookie
+        let sessionToken = Parse.Session.getSessionToken();
+        console.log(Parse.Session.createdAt);
+        document.cookie = `SameSite=strict;expires=${Parse.Session.createdAt.toUTCString()};userToken=${sessionToken}`
+
+        // Hide panel after finished
         hideLoginPanel();
 
     } catch (error) {        
+        console.error(error);
+    }
+}
+
+export async function sessionTokenLogin(token) {
+    try {
+        const user = await Parse.User.become(token);
+    } catch (error) {
         console.error(error);
     }
 }
